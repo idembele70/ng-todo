b2c5736304637ae8a58a9cbe69f5eb9f8f9aa56f
idembele70 <idembele70@gmail>
2025-10-16 07:36:52 +0200

    refactor(todo): remove unused HTTPHeaders import to keep code clean
    Refs: #5

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,8 +1,8 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, of, tap } from 'rxjs';
-import { Todo } from '../models/todo.model';
 import { CompleteTodoEvent } from '../models/complete-todo-event.model';
+import { Todo } from '../models/todo.model';
 
 @Injectable({
   providedIn: 'root'
