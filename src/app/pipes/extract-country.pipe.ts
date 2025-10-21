5b8bfdf756d065fbd272c56325e5ea098e26c00e
idembele70 <idembele70@gmail>
2025-10-21 15:08:33 +0200

    feat(i18n): update extract-country pipe return type to string
    Refs: #23

--- src/app/pipes/extract-country.pipe.ts
+++ src/app/pipes/extract-country.pipe.ts
@@ -6,7 +6,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class ExtractCountryPipe implements PipeTransform {
 
-  transform(value: string): unknown {
+  transform(value: string): string {
     return value.replace(/_.*/, '')
   }
 
