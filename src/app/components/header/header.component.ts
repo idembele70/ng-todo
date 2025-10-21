e356353ff4e1beb682621ff17a00f9cf24e2b5bc
idembele70 <idembele70@gmail>
2025-10-21 15:47:48 +0200

    feat(header): implement AfterViewInit for language selection and improve subscription management
    Refs: #23

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,13 +1,13 @@
 import { AsyncPipe, NgForOf } from "@angular/common";
-import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { FormsModule, NgModel } from "@angular/forms";
 import { TranslatePipe } from '@ngx-translate/core';
-import { catchError, combineLatest, finalize, of, Subject, switchMap, takeUntil } from 'rxjs';
+import { catchError, combineLatest, distinctUntilChanged, finalize, Subject, switchMap, takeUntil } from 'rxjs';
 import { PaginationInfo } from '../../models/paginated-todos.model';
+import { ExtractCountryPipe } from "../../pipes/extract-country.pipe";
 import { LangService } from '../../services/lang.service';
 import { NotificationService } from '../../services/notification.service';
 import { TodoService } from '../../services/todo.service';
-import { ExtractCountryPipe } from "../../pipes/extract-country.pipe";
-import { FormsModule, NgModel } from "@angular/forms";
 
 @Component({
   selector: 'app-header',
@@ -16,7 +16,7 @@ import { FormsModule, NgModel } from "@angular/forms";
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent implements OnInit, OnDestroy {
+export class HeaderComponent implements OnInit, OnDestroy, AfterViewInit {
   isProcessing: boolean = false;
   hasTodos: boolean = false;
   hasCompletedTodos: boolean = false;
@@ -27,6 +27,7 @@ export class HeaderComponent implements OnInit, OnDestroy {
   };
 
   private readonly _destroy$ = new Subject<void>();
+  private readonly _destroyDeleteAllTodoSubscription$ = new Subject<void>();
   readonly currentLang$ = this.langService.currentLang$;
   readonly availableLang$ = this.langService.supportedLang$;
 
@@ -37,33 +38,43 @@ export class HeaderComponent implements OnInit, OnDestroy {
     private readonly notificationService: NotificationService,
     private readonly langService: LangService,
   ) { }
-  
-  ngOnInit(): void {
-    const valueChanges$ = this.langSelectModel.valueChanges ?? this.currentLang$;
 
+  ngAfterViewInit() {
+    if (!this.langSelectModel?.valueChanges) return;
+
+    this.langSelectModel.valueChanges
+      .pipe(takeUntil(this._destroy$))
+      .subscribe(selectedValue => {
+        this.langService.use(selectedValue);
+      })
+  }
+
+  ngOnInit(): void {
     combineLatest([
-      this.todoService.isProcessing$,
-      this.todoService.todos$,
-      this.todoService.hasCompletedTodos$,
-      this.todoService.paginationInfo$,
-      valueChanges$
+      this.todoService.isProcessing$.pipe(distinctUntilChanged()),
+      this.todoService.todos$.pipe(distinctUntilChanged()),
+      this.todoService.hasCompletedTodos$.pipe(distinctUntilChanged()),
+      this.todoService.paginationInfo$.pipe(distinctUntilChanged()),
     ]).pipe(
       takeUntil(this._destroy$),
-    ).subscribe(([isProcessing, todos, hasCompletedTodos, pageInfo, selectedValue]) => {
+    ).subscribe(([isProcessing, todos, hasCompletedTodos, pageInfo]) => {
       this.isProcessing = isProcessing;
       this.hasTodos = !!todos.length;
       this.hasCompletedTodos = hasCompletedTodos;
       this.pageInfo = pageInfo;
-      this.langService.use(selectedValue);
     });
   }
 
   ngOnDestroy(): void {
     this._destroy$.next();
     this._destroy$.complete();
+    this._destroyDeleteAllTodoSubscription$.next();
+    this._destroyDeleteAllTodoSubscription$.complete();
   }
 
   onDeleteAllTodos(completed: boolean) {
+    this._destroyDeleteAllTodoSubscription$.next();
+
     if (!this.hasTodos || this.isProcessing) return;
 
     const prefix = 'header.clear';
@@ -74,7 +85,7 @@ export class HeaderComponent implements OnInit, OnDestroy {
       switchMap(() => this.todoService.refreshTodos(this.pageInfo.currentPage)),
       catchError(() => this.notificationService.notifyError(prefix, key)),
       finalize(() => this.todoService.setProcessing(false)),
-      takeUntil(this._destroy$),
+      takeUntil(this._destroyDeleteAllTodoSubscription$),
     ).subscribe();
   }
 }
